b19b4738ad486c52542d9aa9087a18e14dd96b8b
shreyas-2046 <shreyasjagdale2046@gmail.com>
2023-09-28 13:15:18 +0530

    Changes in cors

--- server/index.js
+++ server/index.js
@@ -21,12 +21,12 @@ database.dbConnect();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://study-notion-edtech.netlify.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+app.use(
+	cors({
+		origin: "*",
+		credentials: true,
+	})
+);
 
 
 app.use(
